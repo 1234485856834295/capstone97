02a8916446dbc1c8d2cb2174fdccfdd0228d8359
1234485856834295 <148546649+1234485856834295@users.noreply.github.com>
2024-01-06 12:16:53 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+answer="bring"
+
+function submit(){
+    if(userpassword=answer){
+        window.location="screen2.html"
+    }
+    else if(userpassword=answer=false){
+        document.getElementById("wrong").innerHTML="wrong"
+    }
+}
